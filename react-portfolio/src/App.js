202e0ce645ab4394ff0559012a34883d6e5951bc
hrcoop16 <hrcoop16@gmail.com>
2022-11-25 18:57:42 -0600

    added footer

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="porfolio">
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-portfolio/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <a href="https://github.com/cooper2016/">Github</a>
+      <a href="https://www.linkedin.com/in/harrison-cooper-108988239/">
+        LinkedIn
+      </a>
+    </div>
+  );
+}
+
+export default Footer;
